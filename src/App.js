047db893c022b1fe624820f1141d2d8056df1c6f
xeangit <mily072@naver.com>
2022-07-02 20:47:36 +0900

    feat: Add Kakao oauth login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes} from 'react-router-dom';
+import KakaoRedirectHandler from './KakaoRedirectHandler';
+import login from './login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="App">
+      <Routes>
+      <Route exact path="/signin" element={login} />
+        <Route path="/oauth/callback/kakao" element={KakaoRedirectHandler} />
+      </Routes>
     </div>
   );
 }
