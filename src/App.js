d2401a2aed62e7f52b6f43585481a8efa9e4a5b8
xeangit <mily072@naver.com>
2022-07-04 00:56:04 +0900

    feat: Modify login logic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-// import { Route, Routes} from 'react-router-dom';
-// import KakaoRedirectHandler from './KakaoRedirectHandler';
-// import login from './login';
-import {KAKAO_AUTH_URL} from './OAuth.js';
+import Login from './Login';
 
 function App() {
   return (
     <div classname="App">
-      {/* <Routes> */}
-      {/* <Route exact path="/signin" element={login} /> */}
-        {/* <Route path="/oauth/callback/kakao" element={KakaoRedirectHandler} /> */}
-      {/* </Routes> */}
-        <div className="kakao_btn">
-           <a href={KAKAO_AUTH_URL}>카카오로 로그인하기</a>
-        </div>
+      <Login />
     </div>
   );
 }
