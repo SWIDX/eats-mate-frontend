505c708e1469ae957be6488e28490518142cd6a6
Queue-ri <qriositylog@gmail.com>
2022-07-12 19:36:28 +0900

    chore: Add `pages` folder

---
+++ src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../components/navigation/NavBar"
+
+function MainPage () {
+
+    return (
+        <>
+            <NavBar/>
+        </>
+    );
+}
+
+export default MainPage;
