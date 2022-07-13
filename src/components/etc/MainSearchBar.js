2cacf811cdad9421d687b16984a91225162fb289
Queue-ri <qriositylog@gmail.com>
2022-07-13 22:50:58 +0900

    feat: Add MainSearchBar component

---
+++ src/components/etc/MainSearchBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./MainSearchBar.module.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function MainSearchBar() {
+
+    return (
+        <>
+        </>
+    );
+}
+
+export default MainSearchBar;
