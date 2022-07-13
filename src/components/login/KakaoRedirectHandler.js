0608030f71756ac247ba444e57efd9aa2244ba5c
Queue-ri <qriositylog@gmail.com>
2022-07-13 22:36:37 +0900

    feat(auth): Dispatch userinfo to redux store

--- src/components/login/KakaoRedirectHandler.js
+++ src/components/login/KakaoRedirectHandler.js
@@ -1,10 +1,16 @@
 import React from "react";
 import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { changeUserInfo } from '../../_actions/user_action';
+import { useNavigate } from 'react-router-dom';
 
 const REST_API_KEY = "c4a648b170fea0fbd26e61d052e9093b";
 const REDIRECT_URI =  "http://localhost:3000/user-service/auth/kakao";
 
 const KakaoRedirectHandler = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
     (async function getAccessToken() {
         let params = new URL(document.location.toString()).searchParams;
         let code = params.get("code"); // 인가코드 받는 부분
@@ -12,7 +18,7 @@ const KakaoRedirectHandler = () => {
         let client_id = REST_API_KEY;
     
         try {
-            const res = await axios.post("https://kauth.kakao.com/oauth/token"
+            const kakao_res = await axios.post("https://kauth.kakao.com/oauth/token"
                 + "?grant_type=" + grant_type
                 + "&client_id=" + client_id
                 + "&redirect_uri=" + REDIRECT_URI
@@ -21,10 +27,11 @@ const KakaoRedirectHandler = () => {
                     headers: {'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'}
                 }
             );
-            console.log(res);
-            await axios.post("http://localhost:8081/user-service/auth/kakao", {
-                access_token: res.data.access_token
+            const res = await axios.post("http://localhost:8081/user-service/auth/kakao", {
+                access_token: kakao_res.data.access_token
             });
+            dispatch(changeUserInfo(res.data))
+            navigate("/");
         } catch(e) {
             console.warn(e);
         }
