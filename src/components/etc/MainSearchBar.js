844085c33e31cf24f594d88de07546f22c5dd215
yeonsu0-0 <107970815+yeonsu0-0@users.noreply.github.com>
2022-07-20 14:29:36 +0900

    feat: Add MainSearchBar
    Co-authored-by: Queue.ri <qriositylog@gmail.com>

--- src/components/etc/MainSearchBar.js
+++ src/components/etc/MainSearchBar.js
@@ -5,8 +5,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function MainSearchBar() {
 
     return (
-        <>
-        </>
+            <div className={styles.banner_menu}>
+            <p>혼밥하기 좋은 식당을 탐색해보세요</p>
+                <div className={styles.search_t}>
+                    <input id="input1" type="text" placeholder="직접 검색하기" className={styles.searchbar} />
+                </div>
+
+                <div className={styles.search_m}>
+                    <div className={styles.map}>
+                        <p>지도에서 찾기</p>
+                    </div>
+                </div>
+            </div>
     );
 }
 
