0bd78544903b1df5f5b16992b555d4d0639a53a3
onyoo <dhsdb02@gmail.com>
2022-07-28 16:09:30 +0900

    feat: Update MapPage routing code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
-import KakaoRedirectHandler from './components/login/KakaoRedirectHandler'
-import MapPage from './pages/MapPage'
-import MapTest from './pages/MapTest';
+import KakaoRedirectHandler from './components/login/KakaoRedirectHandler';
+import MapPage from './pages/MapPage';
 
 function App() {
-    return (
-        <BrowserRouter>
-            <div className="App">
-                <Routes>
-                    <Route exact path="/" element={<MainPage />} />
-                    <Route path="/user-service/auth/kakao" element={<KakaoRedirectHandler />} />
-                    <Route path="/map-service/main" element={<MapPage />} />
-                    <Route path="/map-service/test" element={<MapTest/>}/>
-                </Routes>
-            </div>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<MainPage />} />
+          <Route
+            path="/user-service/auth/kakao"
+            element={<KakaoRedirectHandler />}
+          />
+          <Route path="/map-service/main" element={<MapPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
