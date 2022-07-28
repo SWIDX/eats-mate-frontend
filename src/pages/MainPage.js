06030199816cda2c379f54b22b07ef0b0fb62253
onyoo <dhsdb02@gmail.com>
2022-07-28 15:55:04 +0900

    style: Modify code style

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,15 +1,14 @@
-import React from "react";
-import MainSearchBar from "../components/etc/MainSearchBar";
-import NavBar from "../components/navigation/NavBar"
+import React from 'react';
+import MainSearchBar from '../components/etc/MainSearchBar';
+import NavBar from '../components/navigation/NavBar';
 
-function MainPage () {
-
-    return (
-        <>
-            <NavBar/>
-            <MainSearchBar/>
-        </>
-    );
+function MainPage() {
+  return (
+    <>
+      <NavBar />
+      <MainSearchBar />
+    </>
+  );
 }
 
 export default MainPage;
