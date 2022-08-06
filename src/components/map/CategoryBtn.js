8a02a542e609a06363fa78f78a6bacbac3dc3f46
yyoonnooyy <sss121@swu.ac.kr>
2022-08-06 20:08:21 +0900

    feat(gps): Add gps button action

--- src/components/map/CategoryBtn.js
+++ src/components/map/CategoryBtn.js
@@ -1,33 +1,130 @@
 /* global kakao */
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Map.module.css';
+import axios from 'axios';
 
-function categoryBtn() {
-  function gps() {
-    alert('GPS 선택');
+function categoryBtn(props) {
+
+  const [gpsLoc, setGpsLoc] = useState({
+    lat: 37.56076811229905,
+    lng: 126.93694098263262,
+  });
+  const [border, setBorder] = useState("1px solid #CDCDCD");
+  const [information, setInformation] = useState([]);
+
+  useEffect(() => {
+    props.propFunction(gpsLoc);
+  }, [gpsLoc]);
+
+  /*useEffect(() => {
+    props.propFunction2(information);
+  }, [information]);*/
+
+  function getUserLoc() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setGpsLoc({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+        //props.propFunction(gpsLoc);
+      }, function(error) {
+        console.error(error);
+      }, {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: Infinity
+      });
+    } else {
+      alert('GPS를 지원하지 않습니다');
+    }
   }
+
+  function gps() {
+    getUserLoc();
+  }
+
+  /*function category(gubun) {
+    setBorder('1px solid #e97869');
+    var url = '/map-service/information/findByCategory/';
+      
+    axios.get(url + gubun +'/'+ gpsLoc.lat +'/'+ gpsLoc.lng).then((res) => {
+      console.log(res.data);
+      setInformation(res.data);
+      //props.propFunction2(information);
+    });
+  }*/
+
   return (
     <>
       <div className={styles.category}>
         <ul>
           <li>
-            <button onClick={() => categoryBtn('전체')}>전체</button>
+            <div className={styles.category_btn} style={{border}}>
+              <img
+                alt="category_1"
+                onClick={() => category('전체')}
+                src="/img/category_1.png"
+                width='20'
+                height='30'
+              />
+            </div>
           </li>
           <li>
-            <button onClick={() => categoryBtn('한식')}>한식</button>
+            <div className={styles.category_btn} style={{border}}>
+              <img
+                  alt="category_1"
+                  onClick={() => category('한식')}
+                  src="/img/category_2.png"
+                  width='20'
+                  height='30'
+                />
+            </div>
           </li>
           <li>
-            <button onClick={() => categoryBtn('중식')}>중식</button>
+            <div className={styles.category_btn} style={{border}}>
+              <img
+                  alt="category_1"
+                  onClick={() => category('일식')}
+                  src="/img/category_3.png"
+                  width='20'
+                  height='30'
+                />
+            </div>
           </li>
           <li>
-            <button onClick={() => categoryBtn('일식')}>일식</button>
+            <div className={styles.category_btn} style={{border}}>
+              <img
+                  alt="category_1"
+                  onClick={() => category('중식')}
+                  src="/img/category_4.png"
+                  width='20'
+                  height='30'
+                />
+            </div>
           </li>
           <li>
-            <button onClick={() => categoryBtn('양식')}>양식</button>
+            <div className={styles.category_btn} style={{border}}>
+              <img
+                  alt="category_1"
+                  onClick={() => category('양식')}
+                  src="/img/category_5.png"
+                  width='20'
+                  height='30'
+                />
+            </div>
           </li>
           <li>
-            <button onClick={() => categoryBtn('기타')}>기타</button>
+            <div className={styles.category_btn} style={{border}}>
+              <img
+                  alt="category_1"
+                  onClick={() => category('기타')}
+                  src="/img/category_6.png"
+                  width='20'
+                  height='30'
+                />
+            </div>
           </li>
           <li>
             <img
@@ -35,6 +132,8 @@ function categoryBtn() {
               alt="gps icon"
               onClick={() => gps()}
               src="/img/gps icon.jpg"
+              width='50'
+              height='50'
             />
           </li>
         </ul>
@@ -42,5 +141,4 @@ function categoryBtn() {
     </>
   );
 }
-
 export default categoryBtn;
