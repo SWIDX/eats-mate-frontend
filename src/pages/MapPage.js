6e617fda8e51c2fe3660443c7eea2e8f71e04e60
yyoonnooyy <sss121@swu.ac.kr>
2022-08-06 20:10:16 +0900

    feat: Componentize CategoryBtn, NavBar

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,7 +1,8 @@
 /* global kakao */
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
+import NavBar from '../components/navigation/NavBar';
 import MapContainer from '../components/map/MapContainer';
 import MapSearchBar from '../components/map/MapSearchBar';
 import CategoryBtn from '../components/map/CategoryBtn';
@@ -12,6 +13,10 @@ import InformationCard from '../components/map/InformationCard';
 function MapPage() {
   const [information, setInformation] = useState([]);
   const [clickInformation, setClickInformation] = useState();
+  const [gpsLoc, setGpsLoc] = useState({
+    lat: 0,
+    lng: 0,
+  });
 
   const getLocInfo = (info) => {
     setInformation(info);
@@ -20,11 +25,46 @@ function MapPage() {
     setClickInformation(info);
   };
 
+  const getGpsLoc = (info) => {
+    setGpsLoc({
+      lat: info.lat,
+      lng: info.lng,
+    });
+  };
+
+  function getUserLoc() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        //alert(position.coords.latitude+""+position.coords.longitude);
+        setGpsLoc({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      }, function(error) {
+        console.error(error);
+      }, {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: Infinity
+      });
+    } else {
+      alert('GPS를 지원하지 않습니다');
+    }
+  }
+
+  useEffect(() => {
+    getUserLoc();
+  }, []);
+
   return (
     <>
       <div>
+        <NavBar />
         <MapSearchBar propFunction={getLocInfo} />
-        <CategoryBtn />
+        <CategoryBtn
+          propFunction={getGpsLoc}
+          gpsInformation={gpsLoc}
+        />
         {clickInformation ? (
           <InformationCard clickInformation={clickInformation} />
         ) : null}
@@ -32,6 +72,7 @@ function MapPage() {
       <div className="styles.map">
         <MapContainer
           markerInformation={information}
+          gpsInformation={gpsLoc}
           propFunction={getClickInfo}
         />
       </div>
