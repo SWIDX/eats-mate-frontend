30a89df8c227e4ff09df6ec796481943af1c6462
xeangit <mily072@naver.com>
2022-08-14 16:28:20 +0900

    feat: Add recent review

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import MainSearchBar from '../components/etc/MainSearchBar';
 import NavBar from '../components/navigation/NavBar';
+import RecentReview from "../components/review/RecentReview";
 
 function MainPage() {
   return (
     <>
       <NavBar />
       <MainSearchBar />
+      <RecentReview />
     </>
   );
 }
