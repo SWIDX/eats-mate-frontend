f52a21e427bf24e4962a9db207c4594733b36da7
onyoo <dhsdb02@gmail.com>
2022-08-17 02:58:14 +0900

    feat: Modify restaurant search axios function

--- src/components/map/MapSearchBar.js
+++ src/components/map/MapSearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import styles from './Map.module.css';
 
@@ -6,6 +6,12 @@ const MapSearchBar = (props) => {
   const [inputText, setInputText] = useState('');
   const [selectvalue, setSelectValue] = useState('all');
   const [information, setInformation] = useState([]);
+
+  useEffect(() => {
+    if (information.length != 0) {
+      props.propFunction(information);
+    }
+  }, [information]);
 
   const selectBoxChange = (e) => {
     var value = e.target.value;
@@ -16,56 +22,20 @@ const MapSearchBar = (props) => {
     setInputText(e.target.value);
   };
 
-  const handleOnEnterKeyPress = (e) => {
+  const handleOnEnterKeyPress = async (e) => {
     if (e.key === 'Enter') {
       e.preventDefault();
       if (selectvalue === 'all') {
         var place = inputText;
-        var url = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?serviceKey=iYVYThiw2Vsox6T1%2FhhDhR5iUTuaqxW2AJWXmsDBKqXk7Ct1Z03uYclP8SfSqlJ7%2Fg7LSPv8MRDTmgUKOQBcWw%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&arrange=A&areaCode=1&listYN=Y&keyword=';
-        
-        axios.get(url + place).then((res) => {
-          console.log(res.data);
-        });
-
-      } else if (selectvalue === 'tour') {
-        var place = inputText;
-        var url = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?serviceKey=iYVYThiw2Vsox6T1%2FhhDhR5iUTuaqxW2AJWXmsDBKqXk7Ct1Z03uYclP8SfSqlJ7%2Fg7LSPv8MRDTmgUKOQBcWw%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&arrange=A&areaCode=1&listYN=Y&contentTypeId=12&keyword=';
-      
-        axios.get(url + place).then((res) => {
-          console.log(res.data);
-        });
-
-      } else if (selectvalue === 'culture') {
-        var place = inputText;
-        var url = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?serviceKey=iYVYThiw2Vsox6T1%2FhhDhR5iUTuaqxW2AJWXmsDBKqXk7Ct1Z03uYclP8SfSqlJ7%2Fg7LSPv8MRDTmgUKOQBcWw%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&arrange=A&areaCode=1&listYN=Y&contentTypeId=14&keyword=';
-      
-        axios.get(url + place).then((res) => {
-          console.log(res.data);
-        });
-      
-      } else if (selectvalue === 'event') {
-        var place = inputText;
-        var url = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?serviceKey=iYVYThiw2Vsox6T1%2FhhDhR5iUTuaqxW2AJWXmsDBKqXk7Ct1Z03uYclP8SfSqlJ7%2Fg7LSPv8MRDTmgUKOQBcWw%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&arrange=A&areaCode=1&listYN=Y&contentTypeId=15&keyword=';
-      
-        axios.get(url + place).then((res) => {
-          console.log(res.data);
-        });
-      
-      } else if (selectvalue === 'shopping') {
-        var place = inputText;
-        var url = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?serviceKey=iYVYThiw2Vsox6T1%2FhhDhR5iUTuaqxW2AJWXmsDBKqXk7Ct1Z03uYclP8SfSqlJ7%2Fg7LSPv8MRDTmgUKOQBcWw%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&arrange=A&areaCode=1&listYN=Y&contentTypeId=38&keyword=';
-      
-        axios.get(url + place).then((res) => {
-          console.log(res.data);
-        });
-      
-      } else if (selectvalue === 'name') {
+      } else if (selectvalue === 'restaurant') {
         var restaurant = inputText;
-        var url = 'http://localhost:9001/map-service/information/findByName/';
-        axios.get(url+restaurant).then((res) => {
-          setInformation(res.data);
-          props.propFunction(information);
+        var url = 'http://localhost:8081/map-service/information/findByName/';
+        const data = await axios.get(url + restaurant).then((res) => {
+          return res.data;
         });
+        if (data != undefined || data != []) {
+          setInformation(data);
+        }
       }
       setInputText('');
     }
@@ -89,12 +59,12 @@ const MapSearchBar = (props) => {
   return (
     <div className={styles.search}>
       <select id="select" onChange={selectBoxChange}>
-        <option value="all">전체</option>
-        <option value="tour">관광지</option>
-        <option value="culture">문화시설</option>
-        <option value="event">행사/공연/축제</option>
+        <option value="all">장소</option>
+        <option value="tourist_area">관광지</option>
+        <option value="cultural_facilities">문화시설</option>
+        <option value="festival">행사/공연/축제</option>
         <option value="shopping">쇼핑</option>
-        <option value="name">식당</option>
+        <option value="restaurant">식당</option>
       </select>
       <input
         type="text"
