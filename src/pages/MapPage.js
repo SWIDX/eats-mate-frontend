9a6e7599828c92d1e9da6c2ea6c99e04611a5f2e
onyoo <dhsdb02@gmail.com>
2022-08-17 03:00:32 +0900

    feat: Add ListCard to MapPage

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -6,75 +6,42 @@ import NavBar from '../components/navigation/NavBar';
 import MapContainer from '../components/map/MapContainer';
 import MapSearchBar from '../components/map/MapSearchBar';
 import CategoryBtn from '../components/map/CategoryBtn';
+import InformationCard from '../components/map/InformationCard';
+import ListCard from '../components/map/ListCard';
 
 import styles from './MapPage.module.css';
-import InformationCard from '../components/map/InformationCard';
 
 function MapPage() {
-  const [information, setInformation] = useState([]);
   const [clickInformation, setClickInformation] = useState();
-  const [gpsLoc, setGpsLoc] = useState({
-    lat: 0,
-    lng: 0,
-  });
+  const [listInformation, setListInformation] = useState();
 
-  const getLocInfo = (info) => {
-    setInformation(info);
-  };
   const getClickInfo = (info) => {
     setClickInformation(info);
   };
 
-  const getGpsLoc = (info) => {
-    setGpsLoc({
-      lat: info.lat,
-      lng: info.lng,
-    });
+  const getListInfo = (info) => {
+    setListInformation(info);
   };
-
-  function getUserLoc() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        function (position) {
-          //alert(position.coords.latitude+""+position.coords.longitude);
-          setGpsLoc({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          });
-        },
-        function (error) {
-          console.error(error);
-        },
-        {
-          enableHighAccuracy: true,
-          maximumAge: 0,
-          timeout: Infinity,
-        },
-      );
-    } else {
-      alert('GPS를 지원하지 않습니다');
-    }
-  }
-
-  useEffect(() => {
-    getUserLoc();
-  }, []);
 
   return (
     <>
       <div>
-        <NavBar />
-        <MapSearchBar propFunction={getLocInfo} />
-        <CategoryBtn propFunction={getGpsLoc} gpsInformation={gpsLoc} />
+        <MapSearchBar propFunction={getListInfo} />
+        <CategoryBtn />
+        {listInformation ? (
+          <ListCard
+            listInformation={listInformation}
+            propFunction={getClickInfo}
+          />
+        ) : null}
         {clickInformation ? (
           <InformationCard clickInformation={clickInformation} />
         ) : null}
       </div>
       <div className="styles.map">
         <MapContainer
-          markerInformation={information}
-          gpsInformation={gpsLoc}
-          propFunction={getClickInfo}
+          markerInformation={listInformation}
+          clickedInformation={clickInformation}
         />
       </div>
     </>
