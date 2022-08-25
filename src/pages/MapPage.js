5f34f6373c5f019cbe9a2a09352e16942ab69b1a
yyoonnooyy <sss121@swu.ac.kr>
2022-08-25 19:12:25 +0900

    feat: Add map course function

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from 'react';
 import NavBar from '../components/navigation/NavBar';
 import MapContainer from '../components/map/MapContainer';
 import MapSearchBar from '../components/map/MapSearchBar';
+import MapCourse from '../components/map/MapCourse';
 import CategoryBtn from '../components/map/CategoryBtn';
 import InformationCard from '../components/map/InformationCard';
 import ListCard from '../components/map/ListCard';
@@ -15,6 +16,15 @@ function MapPage() {
   const [clickInformation, setClickInformation] = useState();
   const [listInformation, setListInformation] = useState();
   const [listCardOn, setListCardOn] = useState(false);
+  const [viewCourseComponent, setViewCourseComponent] = useState(false);
+  const [point, setPoint] = useState();
+  const [courseLine, setCourseLine] = useState([/*{
+    lat: "",
+    lng: "",
+    title: "",
+    },*/
+  ]);
+
   const [gpsLoc, setGpsLoc] = useState({
     lat: 0,
     lng: 0,
@@ -30,8 +40,66 @@ function MapPage() {
     setListInformation(info);
   };
 
+  const clickAddCourse = (info) => {
+    setViewCourseComponent(true);
+    setPoint(info);
+  };
+
+  useEffect(() => {
+    /*to set point value directly*/
+  }, [point]);
+
+  const clearCoursePoint = () => {
+    setPoint(null);
+  };
+
+  const drawCourse = (pointInfo) => {
+    setCourseLine([]);
+    setCourseLine(pointInfo);
+  }
+
+  const closeCourseComponent = () => {
+    setViewCourseComponent(false);
+    setCourseLine([]);
+  }
+
+  /*const getGpsLoc = (info) => {
+    setGpsLoc({
+      lat: info.lat,
+      lng: info.lng,
+    });
+  };
+
+  function getUserLoc() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        //alert(position.coords.latitude+""+position.coords.longitude);
+        setGpsLoc({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      }, function(error) {
+        console.error(error);
+      }, {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: Infinity
+      });
+    } else {
+      alert('GPS를 지원하지 않습니다');
+    }
+  }
+
+  useEffect(() => {
+    getUserLoc();
+  }, []);*/
+
   return (
     <>
+      <div>
+        <NavBar />
+      </div>
+
       <div>
         <MapSearchBar propFunction={getListInfo} />
       </div>
@@ -47,9 +115,18 @@ function MapPage() {
         {clickInformation && !listCardOn ? (
           <InformationCard clickInformation={clickInformation} />
         ) : null}
+        {viewCourseComponent ? (
+          <MapCourse point={point}
+          propFunction={clearCoursePoint}
+          propFunction2={drawCourse}
+          propFunction3={closeCourseComponent}/>
+        ) : null}
         <MapContainer
           markerInformation={listInformation}
           clickedInformation={clickInformation}
+          courseLine={courseLine}
+          gpsInformation={gpsLoc}
+          propFunction={clickAddCourse}
         />
       </div>
     </>
