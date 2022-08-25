9f13d0c075c436b9607f3ff1c483c8a67dfee6d1
yyoonnooyy <sss121@swu.ac.kr>
2022-08-25 19:11:21 +0900

    feat: Modify search category

--- src/components/map/MapSearchBar.js
+++ src/components/map/MapSearchBar.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import styles from './Map.module.css';
+import XMLParser from "react-xml-parser"; /*추후 삭제*/
 
 const MapSearchBar = (props) => {
   const [inputText, setInputText] = useState('');
-  const [selectvalue, setSelectValue] = useState('all');
-  const [information, setInformation] = useState([]);
+  const [selectValue, setSelectValue] = useState('전체');
+  const [information, setInformation] = useState([]); // data(restaurant, tour)
 
   useEffect(() => {
     if (information.length != 0) {
@@ -18,61 +19,90 @@ const MapSearchBar = (props) => {
     setSelectValue(value);
   };
 
-  const onChange = (e) => {
+  const onInputTextChange = (e) => {
     setInputText(e.target.value);
   };
+
+  function parseXMLtoJSON(xmlData) {
+    const data = new XMLParser().parseFromString(xmlData).children;
+    setInformation(data[1].children[0].children);
+    props.propFunction(information);
+  }
+
+  /*useEffect(() => {
+    props.propFunction2(tourInformation);
+    //console.log(tourInformation[0]);
+  }, [tourInformation]); // to set tourInformation directly*/
 
   const handleOnEnterKeyPress = async (e) => {
     if (e.key === 'Enter') {
       e.preventDefault();
-      if (selectvalue === 'all') {
+      if (selectValue === '전체') {
         var place = inputText;
-      } else if (selectvalue === 'restaurant') {
-        var restaurant = inputText;
+      } else if (selectValue === '여행지') {
+        var place = inputText;
+        /*url 수정, parseXMLtoJSON도 추후 삭제*/
+        var url = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?serviceKey=iYVYThiw2Vsox6T1%2FhhDhR5iUTuaqxW2AJWXmsDBKqXk7Ct1Z03uYclP8SfSqlJ7%2Fg7LSPv8MRDTmgUKOQBcWw%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&arrange=A&areaCode=1&listYN=Y&contentTypeId=12&keyword=';
+      
+        axios.get(url + place).then((res) => {
+          const data =  res.data;
+          parseXMLtoJSON(data);
+        });
+        /*setInformation(data);
+        props.propFunction(information);*/
+
+      } else if (selectValue === '음식점') {
+          alert("엔터:음식점");
+          /*var restaurant = inputText;
+          var url = 'http://localhost:8081/map-service/information/findByName/';
+          const data = await axios.get(url + restaurant).then((res) => {
+            return res.data;
+          });
+          if (data != undefined || data != []) {
+            setInformation(data);
+            props.propFunction(information);
+          }*/
+      }
+      setInputText('');
+    }
+  }; // input Enter key press event function
+
+  const handleOnKeyPress = async () => {
+    if (selectValue === '전체') {
+      var place = inputText;
+    } else if (selectValue === '여행지') {
+        alert("클릭:여행지");
+    } else if (selectValue === '음식점') {
+        alert("클릭:식당");
+        /*var restaurant = inputText;
         var url = 'http://localhost:8081/map-service/information/findByName/';
         const data = await axios.get(url + restaurant).then((res) => {
           return res.data;
         });
         if (data != undefined || data != []) {
           setInformation(data);
-        }
-      }
-      setInputText('');
-    }
-  }; // input Enter key press event function
-
-  const handleOnKeyPress = () => {
-    if (selectvalue === 'location') {
-      var place = inputText;
-    } else if (selectvalue === 'name') {
-      var restaurant = inputText;
-      var url = 'http://localhost:8081/map-service/information/findByName/';
-      
-      axios.get(url + restaurant).then((res) => {
-        setInformation(res.data);
-        props.propFunction(information);
-      });
+          props.propFunction(information);
+        }*/
     }
     setInputText('');
   }; // search btn key press event function
 
   return (
+
     <div className={styles.search}>
-      <select id="select" onChange={selectBoxChange}>
-        <option value="all">장소</option>
-        <option value="tourist_area">관광지</option>
-        <option value="cultural_facilities">문화시설</option>
-        <option value="festival">행사/공연/축제</option>
-        <option value="shopping">쇼핑</option>
-        <option value="restaurant">식당</option>
-      </select>
+      <button className={styles.dropbtn}>{selectValue}</button>
+        <div className={styles.dropdownContent}>
+            <option value="전체" onClick={selectBoxChange}>전체</option>
+            <option value="여행지" onClick={selectBoxChange}>여행지</option>
+            <option value="음식점" onClick={selectBoxChange}>음식점</option>
+      </div>
       <input
         type="text"
         id="search"
         name="search"
         placeholder="  검색어를 입력하세요."
         value={inputText}
-        onChange={onChange}
+        onChange={onInputTextChange}
         onKeyPress={handleOnEnterKeyPress}
       ></input>
       <img
