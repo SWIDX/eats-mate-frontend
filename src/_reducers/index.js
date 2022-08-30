79b49d4bf3181f250a17917b503cd3046df97493
onyoo <dhsdb02@gmail.com>
2022-08-30 23:33:52 +0900

    feat: Add restReducer

--- src/_reducers/index.js
+++ src/_reducers/index.js
@@ -1,16 +1,19 @@
-import { combineReducers } from 'redux';
-import userReducer from './user_reducer';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { combineReducers } from "redux";
+import userReducer from "./user_reducer";
+import restReducer from "./restaurant_reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
-    key: 'root',
-    storage,
-}
+  key: "root",
+  storage,
+};
 
-const rootReducer = combineReducers({ // reducer 모음
-    userReducer,
-})
+const rootReducer = combineReducers({
+  // reducer 모음
+  userReducer,
+  restReducer,
+});
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
