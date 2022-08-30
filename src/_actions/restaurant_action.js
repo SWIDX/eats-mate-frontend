11d75184443730343c851ab6164cff2878897c2c
onyoo <dhsdb02@gmail.com>
2022-08-30 23:33:07 +0900

    feat: Add restaurant redux action

---
+++ src/_actions/restaurant_action.js
@@ -0,0 +1,20 @@
+export const changeRestInfo = (res) => {
+  return {
+    type: "CHANGE_RESTINFO",
+    payload: res,
+  };
+};
+
+export const chageInfolen = (res) => {
+  return {
+    type: "CHANGE_INFOLEN",
+    payload: res,
+  };
+};
+
+export const changeClickInfo = (res) => {
+  return {
+    type: "CHANGE_CLICKINFO",
+    payload: res,
+  };
+};
