076108eeb928daf267c2fb1cb595377ea9a88355
xeangit <mily072@naver.com>
2022-09-01 20:35:11 +0900

    feat: Modify main search bar

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import MainSearchBar from '../components/etc/MainSearchBar';
 import MainSlideBar from '../components/etc/MainSlideBar';
 import NavBar from '../components/navigation/NavBar';
 import RecommendedList from '../components/etc/RecommendedList';
@@ -10,7 +9,6 @@ function MainPage() {
     <>
       <NavBar />
       <MainSlideBar />
-      <MainSearchBar />
       <RecommendedList />
       <RecentReview />
     </>
