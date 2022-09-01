77227a68cce82bf0ae5be9239139a64b03e4a106
yyoonnooyy <sss121@swu.ac.kr>
2022-09-01 18:41:17 +0900

    feat: Add recommended list

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MainSearchBar from '../components/etc/MainSearchBar';
 import MainSlideBar from '../components/etc/MainSlideBar';
 import NavBar from '../components/navigation/NavBar';
+import RecommendedList from '../components/etc/RecommendedList';
 import RecentReview from "../components/review/RecentReview";
 
 function MainPage() {
@@ -10,6 +11,7 @@ function MainPage() {
       <NavBar />
       <MainSlideBar />
       <MainSearchBar />
+      <RecommendedList />
       <RecentReview />
     </>
   );
