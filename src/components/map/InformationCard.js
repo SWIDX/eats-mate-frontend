582064669294b7907d5975ee3defe88cfedaaa32
yyoonnooyy <sss121@swu.ac.kr>
2022-09-03 22:45:12 +0900

    feat: Add course action

--- src/components/map/InformationCard.js
+++ src/components/map/InformationCard.js
@@ -10,9 +10,19 @@ function InformationCard(props) {
     props.propFunction(onClose);
   }, [onClose]);
 
+  const AddCourse = () => {
+    /*if(props.courseLine.length == 5) {
+      alert("코스 경유지는 최대 5개까지만 추가할 수 있습니다.");
+    } else {*/
+      props.propFunction2(props.clickInformation);
+      //setCourseInfoWindow(false);
+    //}
+  };
+
   return (
     <>
       <div className={styles.card_outer}>
+
         <div className={styles.exitbutton}>
           <button
             onClick={() => {
@@ -22,6 +32,7 @@ function InformationCard(props) {
             X
           </button>
         </div>
+
         <div className={styles.card}>
           <div id="search" name="search">
             <div className={styles.cardTop}>
@@ -29,6 +40,10 @@ function InformationCard(props) {
                 {props.clickInformation.name}{" "}
                 <span>{props.clickInformation.gubun}</span>{" "}
               </div>
+              <button className={styles.heart}>
+                {" "}
+                <img src="/img/heart.png" />
+              </button>
               {/* <div className={styles.cardTag}>광진구</div>  */}
             </div>
 
@@ -38,6 +53,7 @@ function InformationCard(props) {
               <img src="/img/emonga.jpeg" alt="second pic" />
               <img src="/img/emonga.jpeg" alt="third pic" />
             </div>
+
             <div className={styles.divSections}></div>
             <div className={styles.cardMiddle}>
               <div className={styles.addressInfo}>
@@ -64,59 +80,67 @@ function InformationCard(props) {
                   {props.clickInformation.cntct}
                 </div>
               </div>
+
+              <div className={styles.divSections}></div>
+                <div className={styles.reviews}>
+                  평가<span>3건</span>
+                  <div className={styles.more}>더보기</div>
+                  <div className={styles.totalReviews}>
+                    <div className={styles.good}>
+                      <img src="/img/good.png" />
+                      최고예요<span>3</span>
+                    </div>
+                    <div className={styles.soso}>
+                      <img src="/img/soso.png" />
+                      평범해요<span>3</span>
+                    </div>
+                    <div className={styles.bad}>
+                      <img src="/img/bad.png" />
+                      별로예요<span>3</span>
+                    </div>
+                  </div>
+                  <div className={styles.DetailReviews}>
+                    <div className={styles.profile}>
+                      <img src="/img/user1.png" />
+                      나는야먹짱
+                    </div>
+                    <p>
+                      말해뭐해 일단 너무 맛있고요... 혼자 건대 갔다가 들렀는데
+                      혼밥하기 딱 좋은 분위기였어요 추천
+                    </p>
+                    <div className={styles.date}>2022. 01. 08</div>
+                    <div className={styles.face}>
+                      <img src="/img/good.png" />
+                      최고예요
+                    </div>
+                  </div>
+                  <hr />
+                  <div className={styles.DetailReviews}>
+                    <div className={styles.profile}>
+                      <img src="/img/user2.png" />
+                      프로혼밥러
+                    </div>
+                    <p>
+                      와 여기 나만 알던 맛집인데 잇메에도 올라왔네... 강추드려요
+                    </p>
+                    <div className={styles.date}>2022. 01. 08</div>
+                    <div className={styles.face}>
+                      <img src="/img/good.png" />
+                      최고예요
+                    </div>
+                  </div>
+              </div>
             </div>
 
-            <div className={styles.divSections}></div>
-            <div className={styles.cardBottom}>
-              <div className={styles.reviews}>
-                평가<span>3건</span>
-                <div className={styles.more}>더보기</div>
-                <div className={styles.totalReviews}>
-                  <div className={styles.good}>
-                    <img src="/img/good.png" />
-                    최고예요<span>3</span>
-                  </div>
-                  <div className={styles.soso}>
-                    <img src="/img/soso.png" />
-                    평범해요<span>3</span>
-                  </div>
-                  <div className={styles.bad}>
-                    <img src="/img/bad.png" />
-                    별로예요<span>3</span>
-                  </div>
-                </div>
-                <div className={styles.DetailReviews}>
-                  <div className={styles.profile}>
-                    <img src="/img/user1.png" />
-                    나는야먹짱
-                  </div>
-                  <p>
-                    말해뭐해 일단 너무 맛있고요... 혼자 건대 갔다가 들렀는데
-                    혼밥하기 딱 좋은 분위기였어요 추천
-                  </p>
-                  <div className={styles.date}>2022. 01. 08</div>
-                  <div className={styles.face}>
-                    <img src="/img/good.png" />
-                    최고예요
-                  </div>
-                </div>
-                <hr />
-                <div className={styles.DetailReviews}>
-                  <div className={styles.profile}>
-                    <img src="/img/user2.png" />
-                    프로혼밥러
-                  </div>
-                  <p>
-                    와 여기 나만 알던 맛집인데 잇메에도 올라왔네... 강추드려요
-                  </p>
-                  <div className={styles.date}>2022. 01. 08</div>
-                  <div className={styles.face}>
-                    <img src="/img/good.png" />
-                    최고예요
-                  </div>
-                </div>
-              </div>
-              <div className={styles.Buttons}>
+            <div className={styles.Buttons}>
+
+                <img
+                  alt="add course btn"
+                  src="/img/addCourseBtn.png"
+                  width="280px"
+                  height="50px"
+                  onClick={() => AddCourse()}
+                />
                 <button
                   className={styles.DetailButton}
                   onClick={() => {
@@ -127,14 +151,11 @@ function InformationCard(props) {
                 >
                   상세페이지 보기
                 </button>
-                <button className={styles.heart}>
-                  {" "}
-                  <img src="/img/heart.png" />
-                </button>
-              </div>
             </div>
+
           </div>
         </div>
+
       </div>
     </>
   );
