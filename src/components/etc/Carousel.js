f77ca0074c5eddfa4ef8327585a8483b752bab48
Queue-ri <qriositylog@gmail.com>
2022-09-03 12:20:26 +0900

    feat: Add Carousel component

---
+++ src/components/etc/Carousel.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useState, useRef } from 'react';
+import styles from "./Carousel.module.css";
+import { ReactComponent as LeftBtnSvg } from "../../images/svg/carousel-left-button.svg"
+import { ReactComponent as RightBtnSvg } from "../../images/svg/carousel-right-button.svg"
+
+function Carousel(props) {
+    const [slideState, setSlideState] = useState(0);
+    const [timeoutId, setTimeoutId] = useState();
+    
+    const SLIDES_NUM = props.dataList.length - 1;
+
+    const slideRef = useRef(null);
+    const outerViewRef = useRef(null);
+    const innerViewRef = useRef(null);
+    const imageRef = [];
+    const buttonRef = [];
+
+    function imgClick(idx) {
+        // not using useNavigate was intended
+        window.location = props.dataList[idx].link;
+    } // img click event function
+
+    function leftBtnClick() {
+        clearTimeout(timeoutId);
+        if(slideState === 0) {
+            setSlideState(SLIDES_NUM);
+         } else {
+            setSlideState(slideState - 1);
+        }
+    } // left button click event function
+
+    function rightBtnClick() {
+        clearTimeout(timeoutId);
+        if(slideState >= SLIDES_NUM) {
+            setSlideState(0)
+        } else {
+            setSlideState(slideState + 1);
+        }
+    } // right button click event function
+
+    function pointBtnClick(num) {
+        clearTimeout(timeoutId);
+        setSlideState(num);
+    } // point button click event function
+
+    useEffect(()=> {
+        slideRef.current.style.transition = "all 0.8s ease-in-out";
+        outerViewRef.current.style.width = props.outerViewWidth;
+        outerViewRef.current.style.height = props.outerViewHeight;
+        slideRef.current.style.width = props.imageWidth;
+        slideRef.current.style.height = props.imageHeight;
+        innerViewRef.current.style.overflow = props.innerViewOverflow;
+        slideRef.current.style.gap = `${props.gap}px`;
+
+        imageRef.forEach((ref, index) => {
+            ref.style.borderRadius = `${props.imageRadius}px`
+        });
+        buttonRef[0].style.transform = `translateX(-${props.buttonSize/2}px)`
+        buttonRef[1].style.transform = `translateX(${props.buttonSize/2}px)`
+    },[]);
+
+    useEffect(()=> {
+        if (props.scrollStep == "full") {
+            slideRef.current.style.transform = `translateX(-${slideState}00%) translateX(-${slideState * props.gap}px)`;
+        }
+        else if (props.scrollStep == "one") {
+            slideRef.current.style.transform = `translateX(-${slideState * props.imageWidth.replace("px","")}px) translateX(-${slideState * props.gap}px)`;
+        }
+        if (props.autoScroll) setTimeoutId(setTimeout(() => rightBtnClick(), 4500));
+    }, [slideState]);
+
+    return (
+        <div className={styles.slideShow}>
+
+            <div className={styles.slideBtnContainer} ref={outerViewRef}>
+                <div className={styles.leftBtn} onClick={() => leftBtnClick()} ref={ref => buttonRef[0] = ref}>
+                    <LeftBtnSvg width={`${props.buttonSize}px`} height={`${props.buttonSize}px`} />
+                </div>
+                <div className={styles.rightBtn} onClick={() => rightBtnClick()} ref={ref => buttonRef[1] = ref}>
+                    <RightBtnSvg width={`${props.buttonSize}px`} height={`${props.buttonSize}px`} />
+                </div>
+
+                <div className={styles.slideContainer} ref={innerViewRef}>
+                    <div className={styles.slides} ref={slideRef}>
+                        {props.dataList.map((data, index) => {
+                            return <img className={styles.image} key={index} alt="slide img" onClick={() => imgClick(index)} src={data.image} ref={ref => imageRef[index] = ref} />
+                        })}
+                    </div>
+                </div>
+            </div>
+
+            {props.showBullets ?
+            <div className={styles.bullets}>
+                {props.dataList.map((image, index) => {
+                    return <div className={slideState == index ? styles.checked : styles.unchecked} onClick={() => pointBtnClick(index)} />
+                })}
+            </div>
+            : null }
+        </div>
+    );
+}
+
+export default Carousel;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import MainSlideBar from '../components/etc/MainSlideBar';
+import Carousel from '../components/etc/Carousel';
 import NavBar from '../components/navigation/NavBar';
 import RecommendedList from '../components/etc/RecommendedList';
 import RecentReview from "../components/review/RecentReview";
 
 function MainPage() {
+  const testData = [
+    {
+      image: "img/main-carousel/main-carousel-1.png",
+      link: "/about"
+    },
+    {
+      image: "img/main-carousel/main-carousel-2.png",
+      link: "/map-service/main"
+    },
+    {
+      image: "img/slideImg1.png",
+      link: "/mypage"
+    }
+  ];
+
   return (
     <>
       <NavBar />
-      <MainSlideBar />
+      <Carousel
+        dataList={testData}
+        outerViewWidth={"900px"}
+        outerViewHeight={"400px"}
+        imageWidth={"100%"}
+        imageHeight={"100%"}
+        imageRadius={15}
+        gap={100}
+        innerViewOverflow={"visible"}
+        buttonSize={70}
+        scrollStep={"full"}
+        autoScroll={true}
+        showBullets={true}
+      />
+      <Carousel
+        dataList={testData}
+        outerViewWidth={"580px"}
+        outerViewHeight={"200px"}
+        imageWidth={"200px"}
+        imageHeight={"200px"}
+        imageRadius={5}
+        gap={40}
+        innerViewOverflow={"hidden"}
+        buttonSize={50}
+        scrollStep={"one"}
+        autoScroll={false}
+        showBullets={false}
+      />
       <RecommendedList />
       <RecentReview />
     </>
