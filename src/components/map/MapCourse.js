94f83c65da267c908cbeadd25dfc6b3d07d52494
yyoonnooyy <sss121@swu.ac.kr>
2022-09-05 02:50:54 +0900

    feat: Modify map course code cleanup

--- src/components/map/MapCourse.js
+++ src/components/map/MapCourse.js
@@ -9,30 +9,30 @@ function MapCourse(props) {
   const [point, setPoint] = useState([]);
   const [title, setTitle] = useState();
   const [placeNameList, setPlaceNameList] = useState([]);
-  const [placeAddressList, setPlaceAddressList] = useState([]); // lat, lng
-  const [distance, setDistance] = useState();
+  const [placeAddressList, setPlaceAddressList] = useState([]);
   const [distanceList, setDistanceList] = useState([]);
   const [finalDistance, setFinalDistance] = useState();
 
   const clickCompleteBtn = () => {
     if(userinfo == null) {
-     //alert("코스를 완성하려면 먼저 로그인을 해주세요");
-      if(placeNameList.length == 1) {
-        alert("코스를 완성하려면 두 개 이상의 경유지가 필요합니다.");
-      } else {
-        console.log(placeNameList);
-        console.log(placeAddressList);
-        console.log(distanceList);
-      }
+      alert("코스를 완성하려면 로그인이 필요합니다.");
+      console.log(placeNameList);
+      console.log(placeAddressList);
+      console.log(distanceList);
     } else {
-      setTitle(userinfo.name+"메이트님의 혼행 코스");
        if(placeNameList.length == 1) {
         alert("코스를 완성하려면 두 개 이상의 경유지가 필요합니다.");
       } else {
+        var finalTitle = "";
+        if(title == undefined) {
+          finalTitle = title;
+        } else {
+          finalTitle = userinfo.name+"메이트님의 혼행 코스";
+        }
+
         var url = "http://localhost:8081/user-service/user/course/";
-        // 추후 title 수정 작업 진행
         var data = {
-          title: title,
+          title: finalTitle,
           placeNameList: placeNameList,
           placeAddressList: placeAddressList,
           distanceList: distanceList,
@@ -47,14 +47,15 @@ function MapCourse(props) {
         });
       } // if-else
     }
-  };
+  }; // save user course
 
   const clickCloseComponentBtn = (info) => {
     setPoint(point.filter(point => point.name == info));
     setPlaceNameList(placeNameList.filter(placeNameList => placeNameList[0] == info));
     setPlaceAddressList(placeAddressList.filter(placeAddressList => placeAddressList[0] == info));
     setDistanceList(distanceList.filter(distanceList => distanceList[0] == 1234567));
-    props.propFunction3();
+
+    props.closeCourseComponent();
   }; // delete all point data
 
   const clickDeleteBtn = (info) => {
@@ -64,8 +65,6 @@ function MapCourse(props) {
     setDistanceList(distanceList.filter(distanceList => distanceList[0] == "1234567"));
 
     setPoint(point.filter(point => point.name !== info.name));
-    //distanceList.length=0;
-    //setDistanceList(distanceList.filter(distanceList => distanceList[index] == info));
   };
 
   const onInputTitleChange = (e) => {
@@ -89,48 +88,36 @@ function MapCourse(props) {
   useEffect(() => {
     if( props.point!==null) {
       var addPoint = {
-        num: props.point.length,
-        lat: props.point.lat,
-        lng: props.point.lng,
-        name: props.point.name,
-        address: props.point.address,
+        num: props.point.information.length,
+        lat: props.point.information.lat,
+        lng: props.point.information.lng,
+        name: props.point.information.name,
+        address: props.point.information.address,
       };
       setPoint([...point, addPoint]);
 
-      setPlaceNameList([...placeNameList, props.point.name]);
+      setPlaceNameList([...placeNameList, props.point.information.name]);
 
-      setPlaceAddressList([...placeAddressList, props.point.address]);
+      setPlaceAddressList([...placeAddressList, props.point.information.address]);
     }
 
-    props.propFunction(); // To clear course point data
+    props.clearCoursePoint(); // To clear(reset) course point data
 
   }, [props.point]);
 
   useEffect(() => {
-      props.propFunction2(point); // To draw course line on the map]
+      props.drawCourse(point); // To draw course line on the map
 
-      //setDistance(0);
       if(point.length >= 2) {
         var resultList = [];
         for(var i=0; i+1< point.length; i++) {
           const result = getDistanceInKm(point[i].lat, point[i].lng, point[i+1].lat, point[i+1].lng);
           const resultToMeter = result * 1000;
           resultList[i] = parseInt(resultToMeter);
-          //setDistanceList([...distanceList, parseInt(resultToMeter)]);
         }
         setDistanceList(resultList);
-      } //else if(point.length == 1) {
-        //setDistance(0);
-      //}
+      }
   }, [point]);
-  //const result = getDistanceInKm(point[point.length-1].lat, point[point.length-1].lng, point[point.length-2].lat, point[point.length-2].lng);
-
-  useEffect(() => {
-    if(distance !== undefined) {
-      console.log("test:" + distance);
-      setDistanceList([...distanceList, distance]);
-    }
-  }, [distance]);
 
   // to express all course distance;
   useEffect(() => {
@@ -157,15 +144,19 @@ function MapCourse(props) {
               onChange={onInputTitleChange} />
             <img src="/img/rice.png"
               style={{
+                width:"25px",
+                height:"25px",
                 position: "absolute",
-                right: "210px",
+                right: "200px",
                 top: "22.5px",
               }}/>
             <img src="/img/courseBtn.png"
               style={{
+                width:"120px",
+                height:"30px",
                 position: "absolute",
                 right: "60px",
-                top: "15px",
+                top: "20px",
               }}
               onClick={() => clickCompleteBtn()}/>
             <div className={styles.courseDistance}>
@@ -174,12 +165,12 @@ function MapCourse(props) {
 
             <img src="/img/closeBtn.png"
               alt="close btn"
-              width="35"
-              height="35"
               style={{
+                width:"30px",
+                height:"30px",
                 position: "absolute",
-                right: "20px",
-                top: "15px",
+                right: "15px",
+                top: "20px",
               }}
               onClick={() => clickCloseComponentBtn("all delete")}/>
         </div>
