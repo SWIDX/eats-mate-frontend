5d58d484d260c2c081b10e7e5b1cf1c59748cff2
onyoo <dhsdb02@gmail.com>
2022-09-05 00:55:57 +0900

    feat: Modify props code

--- src/components/map/InformationCard.js
+++ src/components/map/InformationCard.js
@@ -1,164 +1,149 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router";
-import styles from "./InformationCard.module.css";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import styles from './InformationCard.module.css';
 
 function InformationCard(props) {
-  const [onClose, setOnClose] = useState(false);
-  const navigate = useNavigate();
+    const [onClose, setOnClose] = useState(false);
+    const [information, setInformation] = useState({});
+    const [type, setType] = useState();
 
-  useEffect(() => {
-    props.propFunction(onClose);
-  }, [onClose]);
+    const navigate = useNavigate();
 
-  const AddCourse = () => {
-    /*if(props.courseLine.length == 5) {
+    useEffect(() => {
+        props.propFunction(onClose);
+    }, [onClose]);
+
+    useEffect(() => {
+        setInformation(props.clickInformation.information);
+        setType(props.clickInformation.type);
+    }, [props.clickInformation]);
+
+    const AddCourse = () => {
+        /*if(props.courseLine.length == 5) {
       alert("코스 경유지는 최대 5개까지만 추가할 수 있습니다.");
     } else {*/
-      props.propFunction2(props.clickInformation);
-      //setCourseInfoWindow(false);
-    //}
-  };
+        props.propFunction2(props.clickInformation);
+        //setCourseInfoWindow(false);
+        //}
+    };
 
-  return (
-    <>
-      <div className={styles.card_outer}>
-
-        <div className={styles.exitbutton}>
-          <button
-            onClick={() => {
-              setOnClose(true);
-            }}
-          >
-            X
-          </button>
-        </div>
-
-        <div className={styles.card}>
-          <div id="search" name="search">
-            <div className={styles.cardTop}>
-              <div className={styles.cardName}>
-                {props.clickInformation.name}{" "}
-                <span>{props.clickInformation.gubun}</span>{" "}
-              </div>
-              <button className={styles.heart}>
-                {" "}
-                <img src="/img/heart.png" />
-              </button>
-              {/* <div className={styles.cardTag}>광진구</div>  */}
-            </div>
-
-            <hr />
-            <div className={styles.cardImg}>
-              <img src="/img/emonga.jpeg" alt="first pic" />
-              <img src="/img/emonga.jpeg" alt="second pic" />
-              <img src="/img/emonga.jpeg" alt="third pic" />
-            </div>
-
-            <div className={styles.divSections}></div>
-            <div className={styles.cardMiddle}>
-              <div className={styles.addressInfo}>
-                {" "}
-                <img src="/img/address.png" /> 주소
-                <div className={styles.addressDetail}>
-                  {props.clickInformation.address}
+    return (
+        <>
+            <div className={styles.card_outer}>
+                <div className={styles.exitbutton}>
+                    <button
+                        onClick={() => {
+                            setOnClose(true);
+                        }}
+                    >
+                        X
+                    </button>
                 </div>
-              </div>
-              <hr />
-              <div className={styles.timeInfo}>
-                {" "}
-                <img src="/img/time.png" /> 영업시간
-                <div className={styles.timeDetail}>
-                  {props.clickInformation.usage_of_week_and_time}
+
+                <div className={styles.card}>
+                    <div id="search" name="search">
+                        <div className={styles.cardTop}>
+                            <div className={styles.cardName}>
+                                {information.name} <span>{information.gubun}</span>{' '}
+                            </div>
+                            <button className={styles.heart}>
+                                {' '}
+                                <img src="/img/heart.png" />
+                            </button>
+                            {/* <div className={styles.cardTag}>광진구</div>  */}
+                        </div>
+
+                        <hr />
+                        <div className={styles.cardImg}>
+                            <img src="/img/emonga.jpeg" alt="first pic" />
+                            <img src="/img/emonga.jpeg" alt="second pic" />
+                            <img src="/img/emonga.jpeg" alt="third pic" />
+                        </div>
+
+                        <div className={styles.divSections}></div>
+                        <div className={styles.cardMiddle}>
+                            <div className={styles.addressInfo}>
+                                {' '}
+                                <img src="/img/address.png" /> 주소
+                                <div className={styles.addressDetail}>{information.address}</div>
+                            </div>
+                            <hr />
+                            <div className={styles.timeInfo}>
+                                {' '}
+                                <img src="/img/time.png" /> 영업시간
+                                <div className={styles.timeDetail}>{information.usage_of_week_and_time}</div>
+                            </div>
+                            <hr />
+                            <div className={styles.callInfo}>
+                                {' '}
+                                <img src="/img/call.png" /> 전화번호
+                                <div className={styles.callDetail}> {information.cntct}</div>
+                            </div>
+
+                            <div className={styles.divSections}></div>
+                            <div className={styles.reviews}>
+                                평가<span>3건</span>
+                                <div className={styles.more}>더보기</div>
+                                <div className={styles.totalReviews}>
+                                    <div className={styles.good}>
+                                        <img src="/img/good.png" />
+                                        최고예요<span>3</span>
+                                    </div>
+                                    <div className={styles.soso}>
+                                        <img src="/img/soso.png" />
+                                        평범해요<span>3</span>
+                                    </div>
+                                    <div className={styles.bad}>
+                                        <img src="/img/bad.png" />
+                                        별로예요<span>3</span>
+                                    </div>
+                                </div>
+                                <div className={styles.DetailReviews}>
+                                    <div className={styles.profile}>
+                                        <img src="/img/user1.png" />
+                                        나는야먹짱
+                                    </div>
+                                    <p>말해뭐해 일단 너무 맛있고요... 혼자 건대 갔다가 들렀는데 혼밥하기 딱 좋은 분위기였어요 추천</p>
+                                    <div className={styles.date}>2022. 01. 08</div>
+                                    <div className={styles.face}>
+                                        <img src="/img/good.png" />
+                                        최고예요
+                                    </div>
+                                </div>
+                                <hr />
+                                <div className={styles.DetailReviews}>
+                                    <div className={styles.profile}>
+                                        <img src="/img/user2.png" />
+                                        프로혼밥러
+                                    </div>
+                                    <p>와 여기 나만 알던 맛집인데 잇메에도 올라왔네... 강추드려요</p>
+                                    <div className={styles.date}>2022. 01. 08</div>
+                                    <div className={styles.face}>
+                                        <img src="/img/good.png" />
+                                        최고예요
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={styles.Buttons}>
+                            <img alt="add course btn" src="/img/addCourseBtn.png" width="280px" height="50px" onClick={() => AddCourse()} />
+                            <button
+                                className={styles.DetailButton}
+                                onClick={() => {
+                                    navigate('/store-detail/main', {
+                                        state: props.clickInformation,
+                                    });
+                                }}
+                            >
+                                상세페이지 보기
+                            </button>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <hr />
-              <div className={styles.callInfo}>
-                {" "}
-                <img src="/img/call.png" /> 전화번호
-                <div className={styles.callDetail}>
-                  {" "}
-                  {props.clickInformation.cntct}
-                </div>
-              </div>
-
-              <div className={styles.divSections}></div>
-                <div className={styles.reviews}>
-                  평가<span>3건</span>
-                  <div className={styles.more}>더보기</div>
-                  <div className={styles.totalReviews}>
-                    <div className={styles.good}>
-                      <img src="/img/good.png" />
-                      최고예요<span>3</span>
-                    </div>
-                    <div className={styles.soso}>
-                      <img src="/img/soso.png" />
-                      평범해요<span>3</span>
-                    </div>
-                    <div className={styles.bad}>
-                      <img src="/img/bad.png" />
-                      별로예요<span>3</span>
-                    </div>
-                  </div>
-                  <div className={styles.DetailReviews}>
-                    <div className={styles.profile}>
-                      <img src="/img/user1.png" />
-                      나는야먹짱
-                    </div>
-                    <p>
-                      말해뭐해 일단 너무 맛있고요... 혼자 건대 갔다가 들렀는데
-                      혼밥하기 딱 좋은 분위기였어요 추천
-                    </p>
-                    <div className={styles.date}>2022. 01. 08</div>
-                    <div className={styles.face}>
-                      <img src="/img/good.png" />
-                      최고예요
-                    </div>
-                  </div>
-                  <hr />
-                  <div className={styles.DetailReviews}>
-                    <div className={styles.profile}>
-                      <img src="/img/user2.png" />
-                      프로혼밥러
-                    </div>
-                    <p>
-                      와 여기 나만 알던 맛집인데 잇메에도 올라왔네... 강추드려요
-                    </p>
-                    <div className={styles.date}>2022. 01. 08</div>
-                    <div className={styles.face}>
-                      <img src="/img/good.png" />
-                      최고예요
-                    </div>
-                  </div>
-              </div>
             </div>
-
-            <div className={styles.Buttons}>
-
-                <img
-                  alt="add course btn"
-                  src="/img/addCourseBtn.png"
-                  width="280px"
-                  height="50px"
-                  onClick={() => AddCourse()}
-                />
-                <button
-                  className={styles.DetailButton}
-                  onClick={() => {
-                    navigate("/store-detail/main", {
-                      state: props.clickInformation,
-                    });
-                  }}
-                >
-                  상세페이지 보기
-                </button>
-            </div>
-
-          </div>
-        </div>
-
-      </div>
-    </>
-  );
+        </>
+    );
 }
 
 export default InformationCard;
