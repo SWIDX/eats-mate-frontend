a920428b7042fb3e35021d6ce7dd3bcbdf62f00d
onyoo <dhsdb02@gmail.com>
2022-09-05 00:56:43 +0900

    feat: Modify clickedInformation props

--- src/components/map/ListCard.js
+++ src/components/map/ListCard.js
@@ -1,73 +1,69 @@
-import React, { useState, useEffect } from "react";
-import TabMenu from "./TabMenu";
-import styles from "./ListCard.module.css";
-import InformationCard from "./InformationCard";
+import React, { useState, useEffect } from 'react';
+import TabMenu from './TabMenu';
+import styles from './ListCard.module.css';
+import InformationCard from './InformationCard';
 
 function ListCard(props) {
-  const [information, setInformation] = useState();
-  const [onClose, setOnClose] = useState(true);
+    const [information, setInformation] = useState();
+    const [onClose, setOnClose] = useState(true);
 
-  const mouseOver = (e) => {
-    e.preventDefault();
-    e.currentTarget.style.backgroundColor = "#ccc";
-  };
-  const mouseLeave = (e) => {
-    e.preventDefault();
-    e.currentTarget.style.backgroundColor = "#fff";
-  };
+    const mouseOver = (e) => {
+        e.preventDefault();
+        e.currentTarget.style.backgroundColor = '#ccc';
+    };
+    const mouseLeave = (e) => {
+        e.preventDefault();
+        e.currentTarget.style.backgroundColor = '#fff';
+    };
 
-  const getSelectedInformation = (information) => {
-    setInformation(information);
-  };
+    const getOnClose = (close) => {
+        setOnClose(close);
+    };
 
-  const getOnClose = (close) => {
-    setOnClose(close);
-  };
+    const setClickedInformation = (res) => {
+        setInformation(res);
+        props.getClickInformation(res);
+    }; // click information props
 
-  useEffect(() => {
-    if (information != undefined) {
-      if (onClose == true) {
-        setOnClose(false);
-      } else {
-        setOnClose(true);
-      }
-    }
-  }, [information]);
+    useEffect(() => {
+        if (information != undefined) {
+            if (onClose == true) {
+                setOnClose(false);
+            } else {
+                setOnClose(true);
+            }
+        }
+    }, [information]);
 
-  useEffect(() => {
-    if (props.listInformation) {
-      setOnClose(true);
-    }
-  }, [props.listInformation]);
+    useEffect(() => {
+        if (props.listInformation) {
+            setOnClose(true);
+        }
+    }, [props.listInformation]);
 
-  return (
-    <>
-      {onClose ? (
-        <div className={styles.list_outer}>
-          <div className={styles.cardTop}>
-            <span className={styles.result_name}>검색결과</span>
-            <span className={styles.result_tag}>
-              {props.listInformation.length}건
-            </span>
-          </div>
+    return (
+        <>
+            {onClose ? (
+                <div className={styles.list_outer}>
+                    <div className={styles.cardTop}>
+                        <span className={styles.result_name}>검색결과</span>
+                        <span className={styles.result_tag}>{props.listInformation.length}건</span>
+                    </div>
 
-          <div className={styles.tabmenu_outer}>
-            <TabMenu
-              className={styles.tabmenu}
-              selectedType={props.selectedType}
-              information={props.listInformation}
-              propFunction={getSelectedInformation}
-            />
-          </div>
-        </div>
-      ) : (
-        <InformationCard
-          clickInformation={information}
-          propFunction={getOnClose}
-        />
-      )}
-    </>
-  );
+                    <div className={styles.tabmenu_outer}>
+                        <TabMenu
+                            className={styles.tabmenu}
+                            selectedType={props.selectedType}
+                            information={props.listInformation}
+                            setClickedInformation={setClickedInformation}
+                        />
+                    </div>
+                </div>
+            ) : (
+                <InformationCard clickInformation={information} propFunction={getOnClose} />
+            )}
+        </>
+    );
 }
 
 export default ListCard;
