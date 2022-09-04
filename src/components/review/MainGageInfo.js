ba4d6b0626b4f67472aa930ab60c4988f1834c50
Queue-ri <qriositylog@gmail.com>
2022-09-05 07:51:15 +0900

    fix: Delete unused component from MainGageInfo

--- src/components/review/MainGageInfo.js
+++ src/components/review/MainGageInfo.js
@@ -9,54 +9,52 @@ function MainGageInfo(props){
 
     return(
         <>
-         <NavBar />
             <div className={styles.responsivewrapper}>
- 
-    
+                
+                <div className={styles.locationboxflex}>
+                    <img className={styles.maingageimg} alt="" src="/img/reviewinfoimg.png"></img>
+                    <br></br>
+                    <div className={styles.locationboxflex}>
+                    
+                    </div>
+                        <div className={styles.gagerightsquarebox}>
                         <div className={styles.locationboxflex}>
-                            <img className={styles.maingageimg} alt="" src="/img/reviewinfoimg.png"></img>
-                            <br></br>
-                            <div className={styles.locationboxflex}>
-                            
-                            </div>
-                                <div className={styles.gagerightsquarebox}>
+                        <div className={styles.gagecircle} >
+                            <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg"><circle  cx="11" cy="11" r="11" fill="#E97869"/></svg>
+                        </div>
+                        <div className={styles.locationboxflex}>
+                        <span className={styles.gagename}>{props.information.name}{" "}</span>
+                        <span className={styles.gagegubun}>{props.information.gubun}</span>
+                        </div>
+                        <div className={styles.likebutton}><LikeButton></LikeButton></div>
+                        </div>
+                            <div className={styles.gagecontents}>
                                 <div className={styles.locationboxflex}>
-                                <div className={styles.gagecircle} >
-                                    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg"><circle  cx="11" cy="11" r="11" fill="#E97869"/></svg>
+                                    <img className={styles.gageimg} alt="" src="/img/location.png"></img>
+                                    <p className={styles.gageinfoname}>주소</p>
                                 </div>
-                                <div className={styles.locationboxflex}>
-                                <span className={styles.gagename}>{props.information.name}{" "}</span>
-                                <span className={styles.gagegubun}>{props.information.gubun}</span>
-                                </div>
-                                <div className={styles.likebutton}><LikeButton></LikeButton></div>
-                                </div>
-                                    <div className={styles.gagecontents}>
-                                        <div className={styles.locationboxflex}>
-                                            <img className={styles.gageimg} alt="" src="/img/location.png"></img>
-                                            <p className={styles.gageinfoname}>주소</p>
-                                        </div>
-                                        <p className={styles.gageinfocontent}>{props.information.address}</p>
-                                    </div>
-                                    <div>
-                                      <div className={styles.locationboxflex}>
-                                            <img className={styles.gageimg} alt="" src="/img/call.png"></img>
-                                            <p className={styles.gageinfoname}>전화번호</p>
-                                      </div>
-                                        <p className={styles.gageinfocontent}>{props.information.cntct}</p>
-                                    </div>
-                                    <div>
-                                        <div className={styles.locationboxflex}>
-                                            <img className={styles.gageimg} alt="" src="/img/time.png"></img>
-                                            <p className={styles.gageinfoname}>영업시간</p>
-                                        </div>   
-                                            <p className={styles.gageinfocontent}>{props.information.usage_of_week_and_time}</p>
-                                    </div>
-                                </div>
+                                <p className={styles.gageinfocontent}>{props.information.address}</p>
                             </div>
+                            <div>
+                                <div className={styles.locationboxflex}>
+                                    <img className={styles.gageimg} alt="" src="/img/call.png"></img>
+                                    <p className={styles.gageinfoname}>전화번호</p>
+                                </div>
+                                <p className={styles.gageinfocontent}>{props.information.cntct}</p>
+                            </div>
+                            <div>
+                                <div className={styles.locationboxflex}>
+                                    <img className={styles.gageimg} alt="" src="/img/time.png"></img>
+                                    <p className={styles.gageinfoname}>영업시간</p>
+                                </div>   
+                                    <p className={styles.gageinfocontent}>{props.information.usage_of_week_and_time}</p>
+                            </div>
+                        </div>
+                    </div>
 
-                            <div className={styles.carouselsize3}>
+                    <div className={styles.carouselsize3}>
 
-                            </div>                
+                    </div>                
             </div>
         </>
     );
