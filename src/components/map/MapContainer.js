973f8b29ad6bc4aaa56373532b27c0e96c1ff638
Queue-ri <qriositylog@gmail.com>
2022-09-04 17:43:19 +0900

    fix(css): Set Kakao map to fit in screen height

--- src/components/map/MapContainer.js
+++ src/components/map/MapContainer.js
@@ -128,7 +128,7 @@ const MapContainer = (props) => {
                 style={{
                     // 지도의 크기
                     width: '100%',
-                    height: '100vh',
+                    height: 'calc(100vh - 75px)',
                 }}
                 level={2} // 지도의 확대 레벨
                 onCenterChanged={(map) =>
