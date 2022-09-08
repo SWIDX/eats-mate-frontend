faec60cd3b1a0ab03248e3524005bbf8f4fbdc97
xeangit <mily072@naver.com>
2022-09-08 22:39:03 +0900

    feat: Modify RecommendeList to responsive

--- src/components/etc/RecommendedList.js
+++ src/components/etc/RecommendedList.js
@@ -14,39 +14,41 @@ const gubun = "카테고리 변수";
 function RecommendedList() {
 
     return (
-        <div className={styles.recommended_list}>
-            <span>{time}</span>
-            <p>인기있는 맛집을 알려드려요</p>
-            <div className={styles.element_list}>
-                <div className={styles.element}>
-                    <img alt="recommended list img1" src="/img/recommendImg1.png" />
-                    <div className={styles.title}>
-                        <img className={styles.image} alt="location img" src="/img/location_color.png" />
-                        <div className={styles.address}>용산구</div>
+        <>
+            <span className={styles.timeformedia}>{time}</span>
+                <div className={styles.maininfomessage}>인기있는 맛집을 알려드려요</div>
+                    <div className={styles.wrap}>
+                        <div className={styles.element_list}>
+                            <div className={styles.element}>
+                                <img alt="recommended list img1" src="/img/recommendImg1.png" />
+                                <div className={styles.title}>
+                                    <img className={styles.image} alt="location img" src="/img/location_color.png" />
+                                    <div className={styles.address}>용산구</div>
+                                </div>
+                                <div className={styles.name}>앙카라피크닉<span> 기타</span></div>
+                                <div className={styles.ment}>오늘 20명이 <span>찜</span>했어요</div>
+                            </div>
+                            <div className={styles.element}>
+                                <img alt="recommended list img2" src="/img/recommendImg2.png" />
+                                <div className={styles.title}>
+                                    <img className={styles.image} alt="location img" src="/img/location_color.png" />
+                                    <div className={styles.address}>서대문구</div>
+                                </div>
+                                <div className={styles.name}>한끼마끼<span> 일식</span></div>
+                                <div className={styles.ment}>오늘 5명이 <span>리뷰</span>를 남겼어요</div>
+                            </div>
+                            <div className={styles.element}>
+                                <img alt="recommended list img3" src="/img/recommendImg3.png" />
+                                <div className={styles.title}>
+                                    <img className={styles.image} alt="location img" src="/img/location_color.png" />
+                                    <div className={styles.address}>광진구</div>
+                                </div>
+                                <div className={styles.name}>송화산시도삭면<span> 일식</span></div>
+                                <div className={styles.ment}>오늘 <span>조회수</span>가 가장 많아요</div>
+                            </div>
+                        </div>
                     </div>
-                    <div className={styles.name}>앙카라피크닉<span> 기타</span></div>
-                    <div className={styles.ment}>오늘 20명이 <span>찜</span>했어요</div>
-                </div>
-                <div className={styles.element}>
-                    <img alt="recommended list img2" src="/img/recommendImg2.png" />
-                    <div className={styles.title}>
-                        <img className={styles.image} alt="location img" src="/img/location_color.png" />
-                        <div className={styles.address}>서대문구</div>
-                    </div>
-                    <div className={styles.name}>한끼마끼<span> 일식</span></div>
-                    <div className={styles.ment}>오늘 5명이 <span>리뷰</span>를 남겼어요</div>
-                </div>
-                <div className={styles.element}>
-                    <img alt="recommended list img3" src="/img/recommendImg3.png" />
-                    <div className={styles.title}>
-                        <img className={styles.image} alt="location img" src="/img/location_color.png" />
-                        <div className={styles.address}>광진구</div>
-                    </div>
-                    <div className={styles.name}>송화산시도삭면<span> 일식</span></div>
-                    <div className={styles.ment}>오늘 <span>조회수</span>가 가장 많아요</div>
-                </div>
-            </div>
-        </div>
+            </>
     );
 
 }
