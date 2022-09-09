537de3a5930bf55d5c04e0612f2cd5219897d0b9
Queue-ri <qriositylog@gmail.com>
2022-09-09 18:56:02 +0900

    fix: Go back to previous page after login trial

--- src/components/login/KakaoRedirectHandler.js
+++ src/components/login/KakaoRedirectHandler.js
@@ -36,7 +36,7 @@ const KakaoRedirectHandler = () => {
                 }
             );
             dispatch(changeUserInfo(res.data))
-            navigate("/");
+            navigate(-1);
         } catch(e) {
             console.warn(e);
             window.alert("오류가 발생했습니다. 다시 시도해주세요.");
@@ -49,7 +49,7 @@ const KakaoRedirectHandler = () => {
             } catch(e) {
                 console.log(e)
             }
-            navigate("/");
+            navigate(-1);
         }
     })();
 
