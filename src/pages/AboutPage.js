d94f4f0240fe8e44e9901958a341a5fb93302e03
xeangit <mily072@naver.com>
2022-09-11 13:18:57 +0900

    Refactor: Modify img to aboutimg

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -15,7 +15,7 @@ function AboutPage(){
         <div className={styles.scrollContainer}>
         <div className={styles.section}>
         <div className={styles.bgLogo}>
-                <img src="/img/logo-character.svg" alt="banner1 mockup" />
+                <img className={styles.aboutimg} src="/img/logo-character.svg" alt="banner1 mockup" />
             </div>
             <div className={styles.banner1}>
                 <h1>잇츠메이트와 함께
@@ -26,7 +26,7 @@ function AboutPage(){
                 건강한 문화를 만들어가고 있습니다</h6>
                 </div>
                 <div className={styles.macMockup}>
-                    { <img src="/img/mac_mainpage.png" alt="banner1 mockup" /> }
+                    { <img className={styles.aboutimg} src="/img/mac_mainpage.png" alt="banner1 mockup" /> }
                 </div>
             </div>
             <div className={styles.section}>
@@ -34,19 +34,19 @@ function AboutPage(){
                     <div className={styles.numbers}>01</div>
                     <h2>식당 탐색하기</h2>
                     <h6>실시간 인기 맛집과 생생한 후기들을 만나보세요</h6>
-                    <img src="/img/banner2-page.jpg" alt="banner2 page image" />
+                    <img className={styles.aboutimg} src="/img/banner2-page.jpg" alt="banner2 page image" />
                     <div className={styles.floating}>
-                        <img src="/img/float-icons.png" alt="banner2 float icon" />
+                        <img className={styles.aboutimg} src="/img/float-icons.png" alt="banner2 float icon" />
                     </div>
                     <div className={styles.banner2icon}>
                         <div className={styles.icon1}>
-                            <img src="/img/banner2-01.png" alt="banner2 background icon1" />
+                            <img className={styles.aboutimg} src="/img/banner2-01.png" alt="banner2 background icon1" />
                         </div>
                         <div className={styles.icon2}>
-                            <img src="/img/banner2-02.png" alt="banner2 background icon2" />
+                            <img className={styles.aboutimg} src="/img/banner2-02.png" alt="banner2 background icon2" />
                         </div>
                         <div className={styles.icon3}>
-                            <img src="/img/banner2-03.png" alt="banner2 background icon3" />
+                            <img className={styles.aboutimg} src="/img/banner2-03.png" alt="banner2 background icon3" />
                         </div>
                     </div>
                 </div>
@@ -59,10 +59,10 @@ function AboutPage(){
                     가고 싶은 장소들을 골라 직접 코스를 만들고 떠나보세요</h6>
                     <img src="/img/banner3-map.png" alt="banner3 page image" />
                     <div className={styles.icon1}>
-                            <img src="/img/banner3-01.png" alt="banner3 background icon1" />
+                            <img className={styles.aboutimg} src="/img/banner3-01.png" alt="banner3 background icon1" />
                         </div>
                         <div className={styles.icon2}>
-                            <img src="/img/banner3-02.png" alt="banner3 background icon2" />
+                            <img className={styles.aboutimg} src="/img/banner3-02.png" alt="banner3 background icon2" />
                         </div>
                     </div>
                 </div>
@@ -72,15 +72,15 @@ function AboutPage(){
                     <h2>혼행 경험 공유하기</h2>
                     <h6>잇츠 메이트로 장소를 탐색하고 다녀온 혼행 경험을<br />
                         다른 메이트들과 공유하고, 나만의 기록으로 남겨보세요!</h6>
-                    <img src="/img/banner4-mypage.png" alt="banner4 page image" />
+                    <img className={styles.aboutimg} src="/img/banner4-mypage.png" alt="banner4 page image" />
                     <div className={styles.icon1}>
-                            <img src="/img/banner4-01.png" alt="banner2 background icon1" />
+                            <img className={styles.aboutimg} src="/img/banner4-01.png" alt="banner2 background icon1" />
                         </div>
                         <div className={styles.icon2}>
-                            <img src="/img/banner4-02.png" alt="banner2 background icon2" />
+                            <img className={styles.aboutimg} src="/img/banner4-02.png" alt="banner2 background icon2" />
                         </div>
                         <div className={styles.icon3}>
-                            <img src="/img/banner4-03.png" alt="banner2 background icon3" />
+                            <img className={styles.aboutimg} src="/img/banner4-03.png" alt="banner2 background icon3" />
                         </div>
                 </div>
             </div>
