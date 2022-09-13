4277087d4d14a759bb35b958cd5f4f43713fddb6
Queue-ri <qriositylog@gmail.com>
2022-09-14 03:55:27 +0900

    fix: Change login button redirect uri & api key

--- src/components/login/LoginButton.js
+++ src/components/login/LoginButton.js
@@ -3,8 +3,8 @@ import styles from "./Login.module.css";
 import { ReactComponent as KakaoLoginSvg } from "../../images/svg/kakao-login.svg";
 
 
-const REST_API_KEY = "c4a648b170fea0fbd26e61d052e9093b";
-const REDIRECT_URI =  "http://localhost:3000/user-service/auth/kakao";
+const REST_API_KEY = "%REACT_APP_KAKAO_MAP_API_KEY%";
+const REDIRECT_URI =  "http://43.200.16.191:3000/user-service/auth/kakao";
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 function LoginButton() {
