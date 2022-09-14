8aaea77155e5f0a188fb423e0b10b6f2c0457980
Queue-ri <qriositylog@gmail.com>
2022-09-15 04:47:05 +0900

    fix: Change wrong data in RecentReview

--- src/components/review/RecentReview.js
+++ src/components/review/RecentReview.js
@@ -110,7 +110,7 @@ function RecentReview(){
                         <div style={{display: 'flex', marginBottom: '16px'}}>
                             <div className={styles.name}>{currentInfo.name}</div>
                             <div className={styles.border} />
-                            <div className={styles.gugun}>{currentInfo.gugun}</div>
+                            <div className={styles.gubun}>{currentInfo.gubun}</div>
                         </div>
                         <div style={{display: 'flex', marginBottom: '8px'}}>
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
