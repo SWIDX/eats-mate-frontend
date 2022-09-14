967c82cc6b9272ca7e146a47135f26e2f3e27289
yyoonnooyy <sss121@swu.ac.kr>
2022-09-15 01:52:59 +0900

    feat: Add action to navigate to store detail page

--- src/components/etc/RecommendedList.js
+++ src/components/etc/RecommendedList.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from 'react-router';
 import styles from "./RecommendedList.module.css";
 
 const today = new Date();
@@ -13,6 +14,9 @@ const gubun = "카테고리 변수";
 
 function RecommendedList() {
 
+    const [storeInfo, setStoreInfo] = useState({first_store:"앙카라피크닉", second_store:"한끼마끼", third_store:"송화산시도삭면 본점"});
+    const navigate = useNavigate();
+
     return (
         <>
         <div className={styles.parent}>
@@ -21,18 +25,24 @@ function RecommendedList() {
                 <div className={styles.maininfomessage}>인기있는 맛집을 알려드려요</div>
                     <div className={styles.wrap}>
                         <div className={styles.element_list}>
-                            <div className={styles.element}>
+                            <div className={styles.element}
+                                onClick={() => {
+                                    navigate('/detail/' + storeInfo.first_store);
+                            }}>
                                 <img className={styles.recommendimage} alt="recommended list img1" src="/img/recommendImg1.png" />
                                 <div className={styles.recommendContent}>
                                     <div className={styles.title}>
                                         <img className={styles.locationimage} alt="location img" src="/img/location_color.png" />
                                         <div className={styles.address}>용산구</div>
                                     </div>
-                                    <div className={styles.name}>앙카라피크닉<span> 기타</span></div>
+                                    <div className={styles.name}>{storeInfo.first_store}<span> 기타</span></div>
                                     <div className={styles.ment}>오늘 20명이 <span>찜</span>했어요</div>
                                 </div>
                             </div>
-                            <div className={styles.element}>
+                            <div className={styles.element}
+                                onClick={() => {
+                                    navigate('/detail/' + storeInfo.second_store);
+                            }}>
                                 <img className={styles.recommendimage} alt="recommended list img2" src="/img/recommendImg2.png" />
                                 <div className={styles.recommendContent}>
                                     <div className={styles.title}>
@@ -43,7 +53,10 @@ function RecommendedList() {
                                     <div className={styles.ment}>오늘 5명이 <span>리뷰</span>를 남겼어요</div>
                                 </div>
                             </div>
-                            <div className={styles.element}>
+                            <div className={styles.element}
+                                onClick={() => {
+                                    navigate('/detail/' + storeInfo.third_store);
+                            }}>
                                 <img className={styles.recommendimage} alt="recommended list img3" src="/img/recommendImg3.png" />
                                 <div className={styles.recommendContent}>
                                     <div className={styles.title}>
