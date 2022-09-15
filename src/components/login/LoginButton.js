b4899761c99d8c395a337b238569d6385dc110bb
Queue-ri <qriositylog@gmail.com>
2022-09-15 22:10:51 +0900

    fix: Change redirect uri protocol in LoginButton

--- src/components/login/LoginButton.js
+++ src/components/login/LoginButton.js
@@ -4,7 +4,7 @@ import { ReactComponent as KakaoLoginSvg } from "../../images/svg/kakao-login.sv
 
 
 const REST_API_KEY = "c4a648b170fea0fbd26e61d052e9093b";
-const REDIRECT_URI =  "http://eats-mate.com/user-service/auth/kakao";
+const REDIRECT_URI =  "https://eats-mate.com/user-service/auth/kakao";
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 function LoginButton() {
