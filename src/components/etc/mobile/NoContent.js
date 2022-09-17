70a92df0f70dd0210684b1e0d9c0abdaae94b790
xeangit <mily072@naver.com>
2022-09-17 16:43:23 +0900

    chore: Modify mobile WIP img

--- src/components/etc/mobile/NoContent.js
+++ src/components/etc/mobile/NoContent.js
@@ -8,7 +8,7 @@ function NoContent(){
         <>
         <div className={styles.all}>
             <div className={styles.main}>PC 환경에서만 제공하고 있는 기능이예요</div>
-            <img alt="" src="/img/noneimg.png"/>
+            <img alt="" src="/img/noneimg2x.png"/>
             <div className={styles.sub}>잇츠메이트의 다양한 기능을 제한없이 이용하기 위해 <span>PC 버전 접속</span>을 권장드려요:)</div>
             <div className={styles.backButton}>
                 <div>메인페이지로 돌아가기</div>
