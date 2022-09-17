9a74b17bcce3e58566f539c5a2b19e3715e27d7d
Queue-ri <qriositylog@gmail.com>
2022-09-18 01:48:25 +0900

    fix: Replace broken mobile Kakao login image
    - 원래 src 폴더 바깥에 재사용되는 컴포넌트 에셋 이미지 넣으면 안됨
    - re-render 안하는건 상관 없는듯

--- src/components/login/LoginButton.js
+++ src/components/login/LoginButton.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Login.module.css";
 import { ReactComponent as KakaoLoginSvg } from "../../images/svg/kakao-login.svg";
+import MobileKakaoLogin from "../../images/mobile_kakao_login.png"
 
 
 const REST_API_KEY = "c4a648b170fea0fbd26e61d052e9093b";
@@ -12,7 +13,7 @@ function LoginButton() {
     return (
         <a href={KAKAO_AUTH_URL}>
             <KakaoLoginSvg className={styles.kakao_btn} />
-            <img className={styles.mobilelogin} alt="" src="img/mobilelogin.png"/>
+            <img className={styles.mobilelogin} alt="" src={MobileKakaoLogin} />
         </a>
     );
 }
